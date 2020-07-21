6badaf99b6da1c13826249e3b442375a53b331a3
stephen remmi <stephenremmi@gmail.com>
2020-07-21 10:37:39 +0300

    configure database

--- hood_project/settings.py
+++ hood_project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'hood_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.psycopg2',
+        'NAME': 'hood',
+        'USER': 'remmi',
+        'PASSWORD': 'stephen',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
