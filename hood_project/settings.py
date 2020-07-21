c660099fa5fadf8b0704f55cb5de8f21192090cf
stephen remmi <stephenremmi@gmail.com>
2020-07-21 11:31:33 +0300

    tweak code in templates

--- hood_project/settings.py
+++ hood_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hood'
+    'hood',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
